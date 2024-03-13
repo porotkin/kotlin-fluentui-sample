a4ef4f12c528657e36d1d2e27004c71ceb3e0013
porotkin <preload737773@gmail.com>
2024-03-13 11:19:59 +0400

    Add CurrentUserPage component

---
+++ app/src/jsMain/kotlin/team/porotkin/routes/CurrentUserPage.kt
@@ -0,0 +1,36 @@
+package team.porotkin.routes
+
+import fluentui.*
+import react.FC
+import react.dom.html.ReactHTML.div
+import react.dom.html.ReactHTML.p
+import team.porotkin.hooks.useUsers
+
+val CurrentUserPage = FC {
+    val users = useUsers()
+
+    div {
+        InlineDrawer {
+            DrawerHeader {
+                DrawerHeaderTitle {
+                    +"All Users"
+                }
+            }
+
+            DrawerBody {
+                p { +"Drawer Body" }
+            }
+        }
+
+        div {
+            users.forEach {
+                Breadcrumb {
+                    BreadcrumbItem {
+                        +it.name
+                    }
+                    BreadcrumbDivider
+                }
+            }
+        }
+    }
+}
