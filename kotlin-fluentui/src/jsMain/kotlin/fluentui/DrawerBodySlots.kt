aecd5e42c14e1e7d4cbf020a3a0ee47619fea7b3
porotkin <preload737773@gmail.com>
2024-03-13 11:17:47 +0400

    Fix Drawer declarations

--- kotlin-fluentui/src/jsMain/kotlin/fluentui/DrawerBodySlots.kt
+++ kotlin-fluentui/src/jsMain/kotlin/fluentui/DrawerBodySlots.kt
@@ -3,6 +3,8 @@
 
 package fluentui
 
+import react.PropsWithChildren
+
 // unhandled import: ComponentProps from "@fluentui/react-utilities" 
 // unhandled import: ComponentState from "@fluentui/react-utilities" 
 // unhandled import: DialogProps from "@fluentui/react-dialog" 
@@ -13,6 +15,6 @@ package fluentui
 // unhandled import: SlotClassNames from "@fluentui/react-utilities" 
 
 
-sealed external interface DrawerBodySlots {
+sealed external interface DrawerBodySlots : PropsWithChildren {
     var root: Slot
 }
