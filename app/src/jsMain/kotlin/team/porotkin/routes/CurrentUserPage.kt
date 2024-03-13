de389d426ccfe21cc66fdb26704052cdc127783e
porotkin <preload737773@gmail.com>
2024-03-13 11:36:28 +0400

    Add styles for CurrentUserPage

--- app/src/jsMain/kotlin/team/porotkin/routes/CurrentUserPage.kt
+++ app/src/jsMain/kotlin/team/porotkin/routes/CurrentUserPage.kt
@@ -1,15 +1,23 @@
 package team.porotkin.routes
 
 import fluentui.*
+import js.objects.jso
 import react.FC
 import react.dom.html.ReactHTML.div
 import react.dom.html.ReactHTML.p
 import team.porotkin.hooks.useUsers
+import web.cssom.Globals.Companion.inherit
 
 val CurrentUserPage = FC {
     val users = useUsers()
 
     div {
+        style = jso {
+            display = web.cssom.Display.flex
+            width = inherit
+            height = inherit
+        }
+
         InlineDrawer {
             open = true
             separator = true
