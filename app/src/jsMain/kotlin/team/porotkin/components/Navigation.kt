67f54d3d3b1f75f47cf47baeb1cd2bdc860fe57f
porotkin <preload737773@gmail.com>
2024-03-14 21:27:52 +0400

    Add `Navigation` component

---
+++ app/src/jsMain/kotlin/team/porotkin/components/Navigation.kt
@@ -0,0 +1,53 @@
+package team.porotkin.components
+
+import fluentui.Breadcrumb
+import fluentui.BreadcrumbButton
+import fluentui.BreadcrumbDivider
+import fluentui.BreadcrumbItem
+import js.objects.jso
+import react.FC
+import react.dom.html.ReactHTML.div
+import react.router.useParams
+import react.useMemo
+import team.porotkin.hooks.useUsers
+import web.cssom.Display
+import web.cssom.px
+
+val Navigation = FC {
+    val currentUserId = useParams()["userId"]
+    val users = useUsers()
+
+    val currentUser = useMemo(currentUserId, users) {
+        users.find {
+            it.id.toString() == currentUserId
+        }
+    }
+
+
+    div {
+        style = jso {
+            display = Display.flex
+            gap = 25.px
+        }
+
+        Breadcrumb {
+            BreadcrumbItem {
+                BreadcrumbButton {
+                    href = "/"
+
+                    +"Home"
+                }
+            }
+            currentUserId?.let {
+                BreadcrumbDivider {}
+                BreadcrumbItem {
+                    BreadcrumbButton {
+                        href = "/$it"
+
+                        +currentUser?.name.toString()
+                    }
+                }
+            }
+        }
+    }
+}
--- app/src/jsMain/kotlin/team/porotkin/routes/Root.kt
+++ app/src/jsMain/kotlin/team/porotkin/routes/Root.kt
@@ -3,9 +3,11 @@ package team.porotkin.routes
 import react.FC
 import react.router.Outlet
 import team.porotkin.components.Header
+import team.porotkin.components.Navigation
 
 val Root = FC {
     Header()
+    Navigation()
 
     Outlet()
 }
