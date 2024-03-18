d3ca737af280ac3034f59a6c8523ea2b4c192830
porotkin <preload737773@gmail.com>
2024-03-18 18:15:15 +0400

    Add some icons declarations

---
+++ kotlin-fluentui/src/jsMain/kotlin/fluentui/icons/FoldingIcons.kt
@@ -0,0 +1,8 @@
+@file:JsModule("@fluentui/react-icons")
+
+package fluentui.icons
+
+import react.FC
+
+external val ChevronDownRegular: FC<*> = definedExternally
+external val ChevronRightRegular: FC<*> = definedExternally
---
+++ kotlin-fluentui/src/jsMain/kotlin/fluentui/icons/Person.kt
@@ -0,0 +1,8 @@
+@file:JsModule("@fluentui/react-icons")
+
+package fluentui.icons
+
+import react.FC
+import react.PropsWithStyle
+
+external val GlobePersonRegular: FC<PropsWithStyle> = definedExternally
