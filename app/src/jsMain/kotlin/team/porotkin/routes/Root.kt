27872fc5565812ae2b350ccaf562e4b9196e6e03
porotkin <preload737773@gmail.com>
2024-03-18 16:53:28 +0400

    Add parent div

--- app/src/jsMain/kotlin/team/porotkin/routes/Root.kt
+++ app/src/jsMain/kotlin/team/porotkin/routes/Root.kt
@@ -1,13 +1,23 @@
 package team.porotkin.routes
 
+import js.objects.jso
 import react.FC
+import react.dom.html.ReactHTML.div
 import react.router.Outlet
 import team.porotkin.components.Header
 import team.porotkin.components.Navigation
+import web.cssom.pct
 
 val Root = FC {
-    Header()
-    Navigation()
+    div {
+        style = jso {
+            width = 100.pct
+            height = 100.pct
+        }
 
-    Outlet()
+        Header()
+        Navigation()
+
+        Outlet()
+    }
 }
