76d0aa5d2875f96e54d6557c1928425a3f142df4
porotkin <preload737773@gmail.com>
2024-03-18 17:22:23 +0400

    Prettify `UsersPage`

--- app/src/jsMain/kotlin/team/porotkin/routes/UsersPage.kt
+++ app/src/jsMain/kotlin/team/porotkin/routes/UsersPage.kt
@@ -1,21 +1,60 @@
 package team.porotkin.routes
 
-import fluentui.Card
+import fluentui.*
+import js.objects.jso
 import react.FC
+import react.create
 import react.dom.html.ReactHTML
+import react.dom.html.ReactHTML.b
+import react.dom.html.ReactHTML.div
 import react.router.dom.Link
+import react.router.useNavigate
 import team.porotkin.hooks.useUsers
+import web.cssom.Display
+import web.cssom.FlexDirection
 
 val UsersPage = FC {
     val users = useUsers()
+    val navigate = useNavigate()
 
     ReactHTML.div {
-        users.forEach {
+        users.forEach { user ->
             Card {
-                Link {
-                    to = "/${it.id}"
+                CardHeader {
+                    header = Body1.create {
+                        b {
+                            +user.name
+                        }
+                    }
+                    description = Caption1.create {
+                        div {
+                            style = jso {
+                                display = Display.flex
+                                flexDirection = FlexDirection.column
+                            }
+                            div {
+                                +"E-mail: ${user.email}"
+                            }
+                            div {
+                                +"Phone: ${user.phone}"
+                            }
+                            div {
+                                +"Website: "
+                                Link {
+                                    to = user.website
 
-                    +it.name
+                                    +user.website
+                                }
+                            }
+                        }
+                    }
+                }
+                CardFooter {
+                    Button {
+                        onClick = { navigate("/${user.id}") }
+
+                        +"Go to User Page"
+                    }
                 }
             }
         }
