4329efe8900fbca34b026630625d168084db68c4
porotkin <preload737773@gmail.com>
2024-03-18 16:53:17 +0400

    Remove Header's border

--- app/src/jsMain/kotlin/team/porotkin/components/Header.kt
+++ app/src/jsMain/kotlin/team/porotkin/components/Header.kt
@@ -4,14 +4,15 @@ import fluentui.Title1
 import js.objects.jso
 import react.FC
 import react.dom.html.ReactHTML.div
-import web.cssom.*
+import web.cssom.NamedColor
+import web.cssom.pct
+import web.cssom.px
 
 val Header = FC {
     div {
         style = jso {
             width = 100.pct
             height = 48.px
-            border = Border(0.5.px, LineStyle.solid)
             backgroundColor = NamedColor.blueviolet
         }
 
