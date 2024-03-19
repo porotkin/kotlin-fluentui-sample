fbc998fcd7704ff9d1e6c7312852bff16fda9559
porotkin <preload737773@gmail.com>
2024-03-19 15:50:19 +0400

    Add `generateImage()` declaration

--- worker-module/src/jsMain/kotlin/WorkerModule.kt
+++ worker-module/src/jsMain/kotlin/WorkerModule.kt
@@ -1,3 +1,11 @@
-fun main() {
+import web.blob.Blob
 
+@JsModule("js-image-generator")
+external object ImageGenerator {
+    fun generateImage(
+        width: Int,
+        height: Int,
+        quality: Int,
+        callback: (error: Any, data: Blob) -> Unit,
+    ): Nothing
 }
